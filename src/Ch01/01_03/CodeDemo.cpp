906b99b6218bbb491b5f0c8c613ba81b066943da
Longclough <r-swann@sky.com>
2024-06-20 10:06:29 +0000

    chapter 1 completed

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -1,14 +1,15 @@
 // Learning C++ 
 // Challenge 01_03
-// Console Interaction, by Eduardo Corpeño 
+// Console Interaction, by Rob Swann 
 
 #include <iostream>
 #include <string>
 
 int main(){
     std::string str;
+    std::cout << "Please Enter your Name: " << std::flush;
     std::cin >> str;
-    std::cout << str;
+    std::cout << "You Entered: " << str << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
