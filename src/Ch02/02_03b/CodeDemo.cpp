c3692b60139fd3f0c4762d9fb6499476cb726bfd
Longclough <r-swann@sky.com>
2024-06-20 12:41:47 +0000

    02_03b complete

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -1,12 +1,30 @@
 // Learning C++ 
 // Exercise 02_03
-// Using Variables, by Eduardo Corpeño 
+// Using Variables, by Rob Swann 
 
 #include <iostream>
 
+int a, b = 5; // single line comment
+
+/*multi
+line
+comment*/
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
-    
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
+    my_flag = true;
+    std::cout << "flag = " << my_flag << std::endl;
+    std::cout << "a + b = " << a + b << std::endl;
+    std::cout << "b - a = " << b - a << std::endl;
+    unsigned int positive;
+    positive = b - a;
+    std::cout << "b - a (unsigned) = " << positive << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
